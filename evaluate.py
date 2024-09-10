d02e56b20d8475b09264216a0859af1021c7ddc2
Michael Angelo Berrospi <blckmessiah@protonmail.com>
2024-09-10 16:51:49 +0200

    Refactored to use normalizations from normalization file.

--- evaluate.py
+++ evaluate.py
@@ -20,6 +20,7 @@ import graph_matching as GMatch
 import graph_processing as GProc
 import filter_banks as FilterBanks
 import norm_params
+import normalizations as Norms
 
 logger = logging.getLogger(__name__)
 
@@ -287,101 +288,6 @@ def visualize_matches(pre_kpts, post_kpts, img1, img2, true_matches, g1_idxs, g2
 
     # for match in zip(g1_idxs,g2_idxs):
     #     ax.plot([x1, x2], [y1, y2], color=color, linewidth=1)
-
-
-def instance_normalization(feature_matrix, norm_type="zscore"):
-    assert norm_type in ["zscore", "minmax"]
-
-    ft_matrix = feature_matrix.copy()
-
-    if norm_type == "zscore":
-        z_score = StandardScaler()
-        ft_matrix = z_score.fit_transform(ft_matrix)
-        # This was used when coords were not included in zscore norm
-        # ft_matrix = np.concatenate(
-        #     (
-        #         ft_matrix[:, :2],
-        #         z_score.fit_transform(ft_matrix[:, 2:]),
-        #     ),
-        #     axis=1,
-        # )
-    elif norm_type == "minmax":
-        min_max = MinMaxScaler()
-        ft_matrix = min_max.fit_transform(ft_matrix)
-
-    return ft_matrix
-
-
-def distribution_normalization(feature_matrix, norm_type="zscore", fts=1):
-    assert norm_type in ["zscore", "minmax"]
-    assert fts in [1, 2, 3, 4]
-
-    # fts has four ids:
-    # 1: Radius and LM fts
-    # 2: Coordinates and LM fts
-    # 3: Radius and Coordinates
-    # 4: Only LM features
-
-    ft_matrix = feature_matrix.copy()
-
-    new_min = norm_params.distr_min
-    new_max = norm_params.distr_max
-    new_mean = norm_params.distr_mean
-    new_std = norm_params.distr_std
-
-    if norm_type == "zscore":
-
-        # The zcore depends on what features are used
-        # Since our normalization parameter matrix includes all fts
-
-        match fts:
-            case 1:
-                # Not Coords and Radius
-                ft_matrix = (ft_matrix - new_mean[:, 2:]) / new_std[:, 2:]
-            case 2:
-                # Coords and NOT Radius
-                ft_matrix = np.concatenate(
-                    (
-                        (ft_matrix[:, :2] - new_mean[:, :2]) / new_std[:, :2],
-                        (ft_matrix[:, 2:] - new_mean[:, 3:]) / new_std[:, 3:],
-                    ),
-                    axis=1,
-                )
-            case 3:
-                # Coords and Radius
-                ft_matrix = (ft_matrix - new_mean) / new_std
-            case 4:
-                # NOT coords NOT radius
-                ft_matrix = (ft_matrix - new_mean[:, 3:]) / new_std[:, 3:]
-
-    elif norm_type == "minmax":
-        match fts:
-            case 1:
-                # Not Coords and Radius
-                ft_matrix = (ft_matrix - new_min[:, 2:]) / (
-                    new_max[:, 2:] - new_min[:, 2:]
-                )
-            case 2:
-                # Coords and NOT Radius
-                ft_matrix = np.concatenate(
-                    (
-                        (ft_matrix[:, :2] - new_min[:, :2])
-                        / (new_max[:, :2] - new_min[:, :2]),
-                        (ft_matrix[:, 2:] - new_min[:, 3:])
-                        / (new_max[:, 3:] - new_min[:, 3:]),
-                    ),
-                    axis=1,
-                )
-            case 3:
-                # Coords and Radius
-                ft_matrix = (ft_matrix - new_min) / (new_max - new_min)
-            case 4:
-                # NOT coord NOT Radius
-                ft_matrix = (ft_matrix - new_min[:, 3:]) / (
-                    new_max[:, 3:] - new_min[:, 3:]
-                )
-
-    return ft_matrix
 
 
 def bootstrap(data: list, n_splits=100):
@@ -756,18 +662,18 @@ def evaluate_patches(
                 if load_ft_maps:
                     # Only instance based available since distribution parameters weren't calculated
                     # The normalizations are optional
-                    pre_feat_matrix = instance_normalization(pre_feat_matrix)
-                    post_feat_matrix = instance_normalization(post_feat_matrix)
+                    pre_feat_matrix = Norms.instance_normalization(pre_feat_matrix)
+                    post_feat_matrix = Norms.instance_normalization(post_feat_matrix)
                     # pass
                 else:
                     # The normalizations are optional
-                    pre_feat_matrix = instance_normalization(pre_feat_matrix)
-                    post_feat_matrix = instance_normalization(post_feat_matrix)
+                    pre_feat_matrix = Norms.instance_normalization(pre_feat_matrix)
+                    post_feat_matrix = Norms.instance_normalization(post_feat_matrix)
 
-                    # pre_feat_matrix = distribution_normalization(
+                    # pre_feat_matrix = Norms.distribution_normalization(
                     #     pre_feat_matrix, norm_type="minmax", fts=2
                     # )
-                    # post_feat_matrix = distribution_normalization(
+                    # post_feat_matrix = Norms.distribution_normalization(
                     #     post_feat_matrix, norm_type="minmax", fts=2
                     # )
                     # pass
@@ -1296,17 +1202,17 @@ def bootstrap_eval(
                 if load_ft_maps:
                     # Only instance based available since distribution parameters weren't calculated
                     # The normalizations are optional
-                    pre_feat_matrix = instance_normalization(pre_feat_matrix)
-                    post_feat_matrix = instance_normalization(post_feat_matrix)
+                    pre_feat_matrix = Norms.instance_normalization(pre_feat_matrix)
+                    post_feat_matrix = Norms.instance_normalization(post_feat_matrix)
                     # pass
                 else:
-                    pre_feat_matrix = instance_normalization(pre_feat_matrix)
-                    post_feat_matrix = instance_normalization(post_feat_matrix)
+                    pre_feat_matrix = Norms.instance_normalization(pre_feat_matrix)
+                    post_feat_matrix = Norms.instance_normalization(post_feat_matrix)
 
-                    # pre_feat_matrix = distribution_normalization(
+                    # pre_feat_matrix = Norms.distribution_normalization(
                     #     pre_feat_matrix, norm_type="zscore", fts=1
                     # )
-                    # post_feat_matrix = distribution_normalization(
+                    # post_feat_matrix = Norms.distribution_normalization(
                     #     post_feat_matrix, norm_type="zscore", fts=1
                     # )
 
